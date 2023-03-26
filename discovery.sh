8c346cda5364658862f3a36ce9eb00f845dfc420
Jo Shinonome <jo.shinonome@gmail.com>
2023-03-26 21:58:43 +0800

    add notebooks

--- discovery.sh
+++ discovery.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
+test -d kdb-common || git clone https://github.com/BuaBook/kdb-common.git
+
 session="discovery"
 
 tmux new-session -d -s $session
 
-tmux send-keys "q src/q/discovery.q -p 1800" C-m
-
-sleep 1
+tmux split-window -v
 
 tmux split-window -v
 
-tmux send-keys "cd src/go;go run main.go" C-m
+tmux send-keys -t 1 "q src/q/discovery.q -p 1800" C-m
+sleep 1
+tmux send-keys -t 2 "cd src/go;go run main.go" C-m
+
+tmux send-keys -t 3 "q kdb-common/boot.q -p 2190 --load-libs compress,rand,mail,file,tz" C-m
+tmux send-keys -t 3 "\l data/dailypx" C-m
+
+tmux select-layout tiled
 
 tmux a
